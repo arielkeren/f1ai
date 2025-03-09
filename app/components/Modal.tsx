da1356dfff39acfbdfc735c1985bd835ac7ad805
shmil123 <arielkeren2007@gmail.com>
2025-03-09 23:00:14 +0200

    Style the modal component

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,5 +1,5 @@
 import { IoClose } from "react-icons/io5";
-import { PiGearFill } from "react-icons/pi";
+import { ImCheckmark } from "react-icons/im";
 
 type Props = {
   children: React.ReactNode;
@@ -11,19 +11,19 @@ type Props = {
 const Modal: React.FC<Props> = ({ children, title, submit, close }) => (
   <div className="fixed top-0 left-0 h-screen w-screen flex justify-center items-center backdrop-brightness-75 z-10">
     <div className="relative flex flex-col gap-10 items-center bg-white p-5 rounded drop-shadow h-1/2 w-1/2">
-      <button onClick={close} className="absolute top-5 right-5">
+      <button
+        onClick={close}
+        className="absolute top-1 right-1 p-1 rounded transition-colors hover:bg-gray-200"
+      >
         <IoClose className="text-3xl" />
       </button>
-
       <h2 className="text-center font-bold uppercase text-4xl">{title}</h2>
-
       <div className="flex flex-col gap-2">{children}</div>
-
       <button
         onClick={submit}
-        className="flex items-center gap-1 bg-gray-900 text-white px-20 py-3 uppercase rounded transition-colors hover:bg-gray-800"
+        className="flex items-center gap-2 text-xl font-medium bg-gray-900 text-white px-20 py-3 uppercase rounded transition-colors hover:bg-gray-800"
       >
-        <PiGearFill />
+        <ImCheckmark />
         Set
       </button>
     </div>
