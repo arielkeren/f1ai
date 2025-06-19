d4b6e388354bdd89c10aac41ed1724f5e2ac8128
shmil123 <arielkeren2007@gmail.com>
2025-06-19 16:45:13 +0300

    Change links to website

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,36 +2,36 @@ import Image from "next/image";
 
 const Header: React.FC = () => (
   <header className="relative flex justify-between items-center px-5 py-3 bg-primary drop-shadow-xl">
-    <a href="https://f1strategyguide/index.html">
+    <a href="https://f1strategyguide.netlify.app/index.html">
       <Image src="/images/logo.png" alt="" width="200" height="200" />
     </a>
     <nav className="flex justify-center flex-wrap gap-4 text-2xl text-white">
       <a
-        href="https://f1strategyguide/index.html"
+        href="https://f1strategyguide.netlify.app/index.html"
         className="hover:bg-red-500 rounded-xl flex items-center px-2 py-3 justify-center transition-colors"
       >
         Home
       </a>
       <a
-        href="https://f1strategyguide/teams.html"
+        href="https://f1strategyguide.netlify.app/teams.html"
         className="hover:bg-red-500 rounded-xl flex items-center px-2 py-3 justify-center"
       >
         Teams
       </a>
       <a
-        href="https://f1strategyguide/drivers.html"
+        href="https://f1strategyguide.netlify.app/drivers.html"
         className="hover:bg-red-500 rounded-xl flex items-center px-2 py-3 justify-center"
       >
         Drivers
       </a>
       <a
-        href="https://f1strategyguide/circuits.html"
+        href="https://f1strategyguide.netlify.app/circuits.html"
         className="hover:bg-red-500 rounded-xl flex items-center px-2 py-3 justify-center"
       >
         Circuits
       </a>
       <a
-        href="https://f1strategyguide/standings.html"
+        href="https://f1strategyguide.netlify.app/standings.html"
         className="hover:bg-red-500 rounded-xl flex items-center px-2 py-3 justify-center"
       >
         Standings
@@ -43,7 +43,7 @@ const Header: React.FC = () => (
         Strategy
       </a>
       <a
-        href="https://f1strategyguide/memes.html"
+        href="https://f1strategyguide.netlify.app/memes.html"
         className="hover:bg-red-500 rounded-xl flex items-center px-2 py-3 justify-center"
       >
         Memes
