1ac24408674ad046c70dffe6de92cb46d4a75467
shmil123 <arielkeren2007@gmail.com>
2025-06-19 16:27:05 +0300

    Change link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,34 +2,36 @@ import Image from "next/image";
 
 const Header: React.FC = () => (
   <header className="relative flex justify-between items-center px-5 py-3 bg-primary drop-shadow-xl">
-    <Image src="/images/logo.png" alt="" width="200" height="200" />
+    <a href="https://f1strategyguide/index.html">
+      <Image src="/images/logo.png" alt="" width="200" height="200" />
+    </a>
     <nav className="flex justify-center flex-wrap gap-4 text-2xl text-white">
       <a
-        href="index.html"
+        href="https://f1strategyguide/index.html"
         className="hover:bg-red-500 rounded-xl flex items-center px-2 py-3 justify-center transition-colors"
       >
         Home
       </a>
       <a
-        href="teams.html"
+        href="https://f1strategyguide/teams.html"
         className="hover:bg-red-500 rounded-xl flex items-center px-2 py-3 justify-center"
       >
         Teams
       </a>
       <a
-        href="drivers.html"
+        href="https://f1strategyguide/drivers.html"
         className="hover:bg-red-500 rounded-xl flex items-center px-2 py-3 justify-center"
       >
         Drivers
       </a>
       <a
-        href="circuits.html"
+        href="https://f1strategyguide/circuits.html"
         className="hover:bg-red-500 rounded-xl flex items-center px-2 py-3 justify-center"
       >
         Circuits
       </a>
       <a
-        href="standings.html"
+        href="https://f1strategyguide/standings.html"
         className="hover:bg-red-500 rounded-xl flex items-center px-2 py-3 justify-center"
       >
         Standings
@@ -41,7 +43,7 @@ const Header: React.FC = () => (
         Strategy
       </a>
       <a
-        href="memes.html"
+        href="https://f1strategyguide/memes.html"
         className="hover:bg-red-500 rounded-xl flex items-center px-2 py-3 justify-center"
       >
         Memes
